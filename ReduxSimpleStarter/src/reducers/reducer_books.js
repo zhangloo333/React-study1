f0be5c570c137fc4d4ed9642b8d5f6f7a438b0ab
ebookcode123 <ebookcode123@gmail.com>
2017-03-21 17:19:22 -0700

    first update

---
+++ ReduxSimpleStarter/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return[{
+      {title:'Javascript: The Googd parts'},
+      {title:'Harry Potter'},
+      {title:'The Dark Tower'},
+      {title:'Eloquent Ruby'}
+  }]
+}
